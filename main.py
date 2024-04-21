fa7ba7702d86720feb67ffb8525f08b20bef6e3e
Emmanuel Sanchez <fede@fede.website>
2024-04-20 20:20:15 -0600

    Handling intents

--- main.py
+++ main.py
@@ -7,4 +7,9 @@ from responses import get_response
 #Step 0: Load our token from somewhere safe
 load_dotenv()
 TOKEN: Final[str] = os.getenv('DISCORD_TOKEN')
-print(TOKEN)
+# print(TOKEN)
+
+#Step 1: BOT SETUP
+intents: Intents = Intents.default()
+intents.message_content = True
+client: Client = Client(intents=Intents)
